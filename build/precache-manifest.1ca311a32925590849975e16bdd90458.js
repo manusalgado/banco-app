10d9d47ffc3d163c1b67caa7c5d3d17b1ada52b5
Manuel Salgado <manuel.salgado@rappi.com>
2020-05-31 18:29:57 -0500

    first commit

---
+++ build/precache-manifest.1ca311a32925590849975e16bdd90458.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "33ac9bc366cbc37d47b66e1726e8f82f",
+    "url": "/app-banco/index.html"
+  },
+  {
+    "revision": "f570ab594263d1f2b8b1",
+    "url": "/app-banco/static/css/2.829c9cb5.chunk.css"
+  },
+  {
+    "revision": "d6905e9f307224c2ba55",
+    "url": "/app-banco/static/css/main.ef80ec08.chunk.css"
+  },
+  {
+    "revision": "f570ab594263d1f2b8b1",
+    "url": "/app-banco/static/js/2.2f5ae868.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/app-banco/static/js/2.2f5ae868.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d6905e9f307224c2ba55",
+    "url": "/app-banco/static/js/main.7c316ec0.chunk.js"
+  },
+  {
+    "revision": "279396f4e90bd4bc1236",
+    "url": "/app-banco/static/js/runtime-main.35ec3c24.js"
+  }
+]);
